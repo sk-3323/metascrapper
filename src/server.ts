f38f9666563ad257d244ef6796935321e4a4daca
sk-3323 <shubham.v.kaniya30@gmail.com>
2025-07-12 08:52:55 +0530

    vercel changed s

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import { createServer } from "http";
 import puppeteer from "puppeteer";
 import { startServer } from "./createServer";
 import handler from "./api/meta";
+import cors from "cors";
 const app = express();
+app.use(cors({ origin: "*", credentials: true }));
 //@ts-ignore
 app.get("/metadata", handler);
 startServer(app);
